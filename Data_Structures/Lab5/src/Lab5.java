6640714596684ced8656622fc05d57b077fbf647
Soomin Lee <dinggididududu@gmail.com>
2021-11-01 23:35:08 -0400

    initial commit - data structures - lab 5

---
+++ Data_Structures/Lab5/src/Lab5.java
@@ -0,0 +1,12 @@
+
+public class Lab5 {
+
+	public static void main(String[] args) {
+		int[] arr1 = {26,38,34,33,70,11,25,72,14,51};
+		int[] arr2 = {13,77,6,40,95,84,50,35,31,54};
+		int[] arr3 = {88,74,46,86,57,39,85,80,19,92};
+		
+		new BinaryTree(); //start with empty binary search tree
+	}
+
+}
